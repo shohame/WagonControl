9d8a2a2a8931e4e82a8b5c6db0925cb763d7d507
shohame <perry.shoham@gmail.com>
2024-05-18 11:17:43 +0300

    Update file structure and added HC06_BT_Config.

---
+++ HC06_BT_Config/HC06_BT_Config.ino
@@ -0,0 +1,44 @@
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+/*
+AT command for HC-06:
+
+AT+NAMEOXEN-0001
+AT+PIN1234
+AT+BAUD4    // 4-9600, 5-19200, 6—38400, 7—57600, 8—115200
+
+*/
+
+
+const byte rxPin = 3; // connected to TX on BT module
+const byte txPin = 2; // connected to RX on BT module
+
+SoftwareSerial BTSerial(rxPin, txPin); 
+
+void setup() {
+  // define pin modes for tx, rx:
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  BTSerial.begin(9600); //9600, 19200, 38400, 57600, 115200
+  Serial.begin(9600);
+}
+
+String messageBuffer = "";
+String message = "";
+char dataByte;
+
+void loop() {
+
+  while(BTSerial.available()) {
+    dataByte = BTSerial.read();
+    if (dataByte !='S')
+    Serial.write(dataByte);
+  }
+  
+  while(Serial.available()){
+    dataByte = Serial.read();
+    BTSerial.write(dataByte);
+  }
+  
+}
