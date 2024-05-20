7a1b949d65417604f5a350fdec27a2046511703a
shohame <perry.shoham@gmail.com>
2024-05-20 17:10:38 +0300

    continue coding BT_RC_Control...

--- WagonControl/BT_RC_Control.cpp
+++ WagonControl/BT_RC_Control.cpp
@@ -11,13 +11,68 @@ BT_RC_Control::BT_RC_Control(void){
 
 void BT_RC_Control::setup()
 {
-    _speed_factor = 0;
+    _speed_level = 0; // 0 - 10
     _red_light = 0;
     _blue_light = 0;
     _speed_r = 0;
     _speed_l = 0;
 }
 
-void BT_RC_Control::set_char(){
+void BT_RC_Control::set_char(char dataByte){
+
+  if (dataByte>='0' && dataByte<='9'){
+    _speed_level = dataByte - '0';
+  }
+  else 
+  {
+    switch (dataByte){
+      case 'S':         // Stop
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'F':         // Forward
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'B':         // Backward
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'R':         // Right
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'L':         // Left
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'I':       // FR
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'G':       // FL
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'J':       // BR
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'H':       // BL
+        _speed_r = 0;
+        _speed_l = 0;
+        break;
+      case 'q':
+        _speed_level = 10;        
+        break;
+  
+      default:
+
+    }
+  }
+
+     
+)
+
 
 }
