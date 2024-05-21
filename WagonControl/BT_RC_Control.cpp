08bf776dfcb0797689c7de41fb2e65ce2a307e0b
shohame <33593461+shohame@users.noreply.github.com>
2024-05-21 18:11:16 +0300

    Update BT_RC_Control.cpp

--- WagonControl/BT_RC_Control.cpp
+++ WagonControl/BT_RC_Control.cpp
@@ -21,10 +21,11 @@ void BT_RC_Control::setup()
 void BT_RC_Control::set_char(char dataByte){
 
   int forward_speed;
-  forward_speed =  MOTORS_MIN_PWM_VAL_SPEED + 
-  _speed_level 
-MOTORS_MIN_PWM_VAL_SPEED
-MOTORS_MAX_PWM_VAL_SPEED
+  int turn_speed;  
+    
+  forward_speed =  MOTORS_MIN_PWM_VAL_SPEED + (MOTORS_MAX_PWM_VAL_SPEED - MOTORS_MIN_PWM_VAL_SPEED) * _speed_level;
+  turn_speed = forward_speed / TURN_SPEED_FACTOR;
+    
   if (dataByte>='0' && dataByte<='9'){
     _speed_level = dataByte - '0';
   }
@@ -35,40 +36,40 @@ MOTORS_MAX_PWM_VAL_SPEED
         _speed_r = 0;
         _speed_l = 0;
         break;
-      case 'F':         // Forward
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'B':         // Backward
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'R':         // Right
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'L':         // Left
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'I':       // FR
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'G':       // FL
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'J':       // BR
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
-      case 'H':       // BL
-        _speed_r = 0;
-        _speed_l = 0;
-        break;
       case 'q':
         _speed_level = 10;        
+        break;
+      case 'F':         // Forward
+        _speed_r = forward_speed;
+        _speed_l = forward_speed;
+        break;
+      case 'B':         // Backward
+        _speed_r = -forward_speed;
+        _speed_l = -forward_speed;
+        break;
+      case 'R':         // Right
+        _speed_r = -turn_speed;
+        _speed_l = turn_speed;
+        break;
+      case 'L':         // Left
+        _speed_r = turn_speed;
+        _speed_l = -turn_speed;
+        break;
+      case 'I':       // FR
+        _speed_r = forward_speed - turn_speed;
+        _speed_l = forward_speed + turn_speed;
+        break;
+      case 'G':       // FL
+        _speed_r = forward_speed + turn_speed;
+        _speed_l = forward_speed - turn_speed;
+        break;
+      case 'J':       // BR
+        _speed_r = -forward_speed + turn_speed;
+        _speed_l = -forward_speed - turn_speed;
+        break;
+      case 'H':       // BL
+        _speed_r = -forward_speed - turn_speed;
+        _speed_l = -forward_speed + turn_speed;
         break;
   
       default:
