acd216fa569eed04f5db5b888131650727519146
shohame <perry.shoham@gmail.com>
2024-12-20 07:27:59 +0200

    Added comment with programming key words. Switch 4,5 legs tx & rx. Change the BAUD Rate to 38400.

--- HC06_BT_Config/HC06_BT_Config.ino
+++ HC06_BT_Config/HC06_BT_Config.ino
@@ -2,19 +2,36 @@
 #include <SoftwareSerial.h>
 
 /*
-AT command for HC-06:
+AT command for HC-05:
 
-Must set "No Line Ending"
+AT+NAME=XXXX  // Change the BT Name
 
-AT+NAMEOXEN-0001
-AT+PIN1234
-AT+BAUD4    // 4-9600, 5-19200, 6—38400, 7—57600, 8—115200
+AT+ORGL   // Reset to defaults
+AT+RMAAD  // Clear any paired devices
+AT+ROLE=1 // Master mode
+AT+RESET  // Need to restart after changing role:
+AT+CMODE=1  // Allow connecting to any address:
+AT+INQM=0,5,5 // Inquire mode – Standard, stop after 5 devices found, (or) stop searching after 5 seconds:
+AT+PSWD=0000 // Set PIN, should be the same as the slave:
+AT+INIT // Start SPP profile lib (needed to do any Bluetooth transmitting/receiving):
+    Note: If it says Error:(17) then that means it’s already loaded, you can carry on.
+AT+INQ // Start searching:
+
+
+AT+CMODE=0
+AT+BIND=0cfc,83,1f384f
+AT+LINK=0cfc,83,1f384f
+AT+ROLE=1    // set as master
+AT+INIT
+AT+PIN
+
+AT+ STATE
 
 */
 
 
-const byte rxPin = 5; // connected to TX on BT module
-const byte txPin = 4; // connected to RX on BT module
+const byte rxPin = 4; // connected to TX on BT module
+const byte txPin = 5; // connected to RX on BT module
 
 SoftwareSerial BTSerial(rxPin, txPin); 
 
@@ -22,7 +39,7 @@ void setup() {
   // define pin modes for tx, rx:
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  BTSerial.begin(9600); //9600, 19200, 38400, 57600, 115200
+  BTSerial.begin(38400); //9600, 19200, 38400, 57600, 115200
   Serial.begin(9600);
 }
 
@@ -34,7 +51,6 @@ void loop() {
 
   while(BTSerial.available()) {
     dataByte = BTSerial.read();
-    if (dataByte !='S')
     Serial.write(dataByte);
   }
   
